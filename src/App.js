4492cc15fac5e15b0540ad1f664b0165e559d0da
Kevin Perez <kevin.perez@magmalabs.io>
2023-01-05 14:54:44 -0600

    Add brief description and social network links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import logo from './logo.png';
+import appIcon from './app-icon.png';
+import appLogo from './app-logo.png';
+import logos from './logos';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> Playas On Tech - Official Web Site </p>
+      <header>
+        <img src={appIcon} className="App-icon" alt="app icon" />
+        <img src={appLogo} className="App-logo" alt="app logo" />
       </header>
+
+      <main>
+        <p>
+          Somos una comunidad de entusiastas de la tecnología que se reúnen para compartir
+          conocimientos y experiencias
+        </p>
+        <ul className="App-social-networks">
+          <li>
+            <a href="https://www.facebook.com/playasontech">
+              <img src={logos.facebook} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/Playas.OnTech">
+              <img src={logos.instagram} />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/PlayasOnTech">
+              <img src={logos.twitter} />
+            </a>
+          </li>
+        </ul>
+      </main>
+
+      <footer>
+        <span>
+          Powered by <a href="https://magmalabs.io">magmalabs.io</a>
+        </span>
+      </footer>
     </div>
   );
 }
---
+++ src/logos.js
@@ -0,0 +1,9 @@
+import facebook from './logos/facebook.png';
+import instagram from './logos/instagram.svg';
+import twitter from './logos/twitter.svg';
+
+export default {
+  facebook,
+  instagram,
+  twitter
+};
