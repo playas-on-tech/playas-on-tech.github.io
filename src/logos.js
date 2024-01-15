a273a360ecc9593a752ce750e14bb54135354f92
Juanequex <ramon.g.uz37@gmail.com>
2024-01-15 14:39:04 -0600

    set the x new logo

--- src/logos.js
+++ src/logos.js
@@ -1,6 +1,6 @@
 import facebook from './logos/facebook.png';
 import instagram from './logos/instagram.svg';
-import twitter from './logos/twitter.svg';
+import twitter from './logos/x.png';
 
 export default {
   facebook,
