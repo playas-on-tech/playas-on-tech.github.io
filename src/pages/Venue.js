4f6316901b2e3a07bdf89e6afb3bec9f15a0d118
Kevin Perez <kevindperezm@gmail.com>
2025-09-05 13:30:43 -0600

    Update venue to Centro Cultural Salagua

--- src/pages/Venue.js
+++ src/pages/Venue.js
@@ -9,15 +9,16 @@ function Venue() {
         donde se llevará a cabo.
       </p>
       <div className="venue-details">
-        <strong>Dirección:</strong> Calle Ejemplo 123, Colonia Centro, Manzanillo, Colima
+        <strong>Dirección:</strong> Centro Cultural de Salagua, C. Octavio Paz, Nuevo Salahua, 28869
+        Manzanillo, Col.
         <br />
-        <strong>Fecha:</strong> 20 de julio, 2025
+        <strong>Fecha:</strong> 18 de septiembre, 2025
         <br />
-        <strong>Hora:</strong> 18:00 hrs
+        <strong>Hora:</strong> 20:00 hrs
       </div>
       <iframe
         className="venue-map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.123456789!2d-104.3245678!3d19.1134567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8423a123456789ab%3A0x123456789abcdef!2sBoulevard%20Miguel%20de%20la%20Madrid%201234%2C%20Manzanillo%2C%20Colima!5e0!3m2!1ses-419!2smx!4v1680000000000!5m2!1ses-419!2smx"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.865000261917!2d-104.32997418768079!3d19.113577450714022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8424d798106c7d59%3A0x616a65f4792270b6!2sCentro%20Cultural%20de%20Salagua!5e0!3m2!1ses-419!2smx!4v1757100501589!5m2!1ses-419!2smx"
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
