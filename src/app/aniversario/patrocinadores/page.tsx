a06155abb23caf2102da852059e3df8d1716b8b6
Kevin Perez <kevindperezm@gmail.com>
2026-05-30 12:58:27 -0600

    Hide redirect page content

--- src/app/aniversario/patrocinadores/page.tsx
+++ src/app/aniversario/patrocinadores/page.tsx
@@ -1,14 +1,3 @@
 export default function OldSponsorsRedirect() {
-  return (
-    <>
-      <meta httpEquiv="refresh" content="0; url=/patrocinadores" />
-      <p className="flex min-h-screen items-center justify-center bg-navy text-lg text-white/70">
-        Redirigiendo a{" "}
-        <a href="/patrocinadores" className="ml-1 text-ocean-300 underline">
-          /patrocinadores
-        </a>
-        ...
-      </p>
-    </>
-  );
+  return <meta httpEquiv="refresh" content="0; url=/patrocinadores" />;
 }
